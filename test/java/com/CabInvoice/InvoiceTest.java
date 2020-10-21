c91d3c1b57add8b29509aaf42a215460592bec8d
suyash-dce <jain.suyash28@gmail.com>
2020-10-21 17:58:38 +0530

    Modified: Code Modified to Handle Premium Ride Bonus Test Cases

--- test/java/com/CabInvoice/InvoiceTest.java
+++ test/java/com/CabInvoice/InvoiceTest.java
@@ -17,7 +17,8 @@ public class InvoiceTest {
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
 		double distance = 2.5;
 		int time = 10;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		String rideType = "normal";
+		double fare = invoiceGenerator.calculateFare(distance, time, rideType);
 		Assert.assertEquals(35, fare, 0.0);
 	}
 
@@ -25,17 +26,26 @@ public class InvoiceTest {
 	public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
 		double distance = 0.1;
 		int time = 1;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		String rideType = "normal";
+		double fare = invoiceGenerator.calculateFare(distance, time, rideType);
 		Assert.assertEquals(5, fare, 0.0);
 	}
 
 	@Test
-	public void givenMultipleRidesShouldReturnInvoiceSummary() {
+	public void givenUserIdShouldReturnInvoiceSummary() {
 		int userId = 123;
-		Rides[] rides = { new Rides(2.0, 5), new Rides(0.1, 1) };
+		Rides[] rides = { new Rides(2.0, 5, "normal"), new Rides(0.1, 1, "premium") };
 		InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator(userId, rides);
 		Summary actualInvoiceSummary = cabInvoiceGenerator.calculateFare(userId);
-		Summary expectedInvoiceSummary = new Summary(2, 30.0);
+		Summary expectedInvoiceSummary = new Summary(2, 45.0);
 		Assert.assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
+	}
+
+	@Test
+	public void givenMultipleRidesShouldReturnInvoiceSummary() {
+		Rides[] rides = { new Rides(2.0, 5, "normal"), new Rides(0.1, 1, "premium") };
+		Summary invoiceSummary = invoiceGenerator.calculateFare(rides);
+		Summary expectedInvoiceSummary = new Summary(2, 45.0);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
 	}
 }
